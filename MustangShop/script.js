ea988ad6fd6e0a837d3f30ede8e3bbfcfa6ddff4
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2022-05-20 18:35:56 -0300

    :memo: Add local files from class (20/05/2022)

---
+++ MustangShop/script.js
@@ -0,0 +1,6 @@
+function showText () {
+    document.getElementById("sobre").innerHTML = "<p>Preço sugerido: A partir de R$ 532.785</p> <br> <p>Marca: Ford Motor Company</p> <br> <p>Velocidade máxima: 250 km/h</p> <br> <p>Potência: 483 HP</p> <br> <p>Motor: 5,0 l V8</p> <br> <p>Aceleração de 0 a 100 km/h: 4,3 segundos</p> <br> <p>Tanque de combustível: 61 l</p>"
+}
+function toggleText () {
+    
+}
